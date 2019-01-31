f0a0b50008329b7bc0238cc7aced6a9fe2d17e1a
Danilo C. Graciano <danilocgraciano@gmail.com>
2019-01-31 14:48:28 -0200

    Alterado nomes dos métodos, convenção da linguagem

--- games/guessing.py
+++ games/guessing.py
@@ -6,14 +6,14 @@ def play():
     score = 1000
     level = 0
 
-    showWelcomeMessage()
+    show_welcome_message()
 
-    showLevelMessage()
+    show_level_message()
 
     while(level == 0):
-        level = readLevel()
+        level = read_level()
 
-    attempts = getAttempts(level)
+    attempts = calculate_attempts(level)
 
     for round in range(1, attempts + 1):
 
@@ -22,7 +22,7 @@ def play():
 
             kick = 0;
             while(kick == 0):
-                kick = readKick()
+                kick = read_kick()
 
             hit = kick == secret_number
 
@@ -30,28 +30,28 @@ def play():
                 print("Congratulations, you WIN, and your score is {}!".format(score))
                 break
             else:
-                showFeedbackMessage(kick, secret_number)
-                score = score - calculateLostScore(kick, secret_number)
+                show_feedback_message(kick, secret_number)
+                score = score - calculate_lost_score(kick, secret_number)
 
         except ValueError:
             print("Please, type a number...")
 
     print("The End")
 
-def showWelcomeMessage():
+def show_welcome_message():
     print("#####################################")
     print("#     Welcome to Guessing Game      #")
     print("#####################################")
 
-def showLevelMessage():
+def show_level_message():
     print("(1) Easy, (2) Medium, (3) Hard")
 
-def readLevel():
+def read_level():
     try:
         level = int(input("Choose the level: "))
         while (level < 1 or level > 3):
             print("Number invalid...")
-            showLevelMessage()
+            show_level_message()
             level = int(input("Choose the level: "))
     except ValueError:
         print("Please, type a number...")
@@ -59,7 +59,7 @@ def readLevel():
     else:
         return level
 
-def getAttempts(level):
+def calculate_attempts(level):
     if (level == 1):
         attempts = 20
     elif(level == 2):
@@ -68,7 +68,7 @@ def getAttempts(level):
         attempts = 5
     return attempts
 
-def readKick():
+def read_kick():
     try:
         kick = int(input("Type a number between 1 an 100: "))
         while (kick < 1 or kick > 100):
@@ -79,7 +79,7 @@ def readKick():
         print("Please, type a number...")
         return 0;
 
-def showFeedbackMessage(kick, secret_number):
+def show_feedback_message(kick, secret_number):
     print("You missed!")
 
     greater_than = kick < secret_number
@@ -90,7 +90,7 @@ def showFeedbackMessage(kick, secret_number):
     elif (less_than):
         print("The number is smaller!")
 
-def calculateLostScore(kick, secret_number):
+def calculate_lost_score(kick, secret_number):
     return abs((secret_number - kick))
 
 if (__name__ == "__main__"):
