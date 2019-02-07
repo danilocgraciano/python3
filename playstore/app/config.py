db7bd07b8345c8dacc8a06891b3ba1b0b2bed604
Danilo C. Graciano <danilocgraciano@gmail.com>
2019-02-07 15:54:14 -0200

    Ajuste de variáveis.

--- playstore/app/config.py
+++ playstore/app/config.py
@@ -1,13 +1,6 @@
 import os
 
-SECRET_KEY = "mK6W8Vhu7qQCb6hV"
+SECRET_KEY = os.environ.get('SECRET_KEY') or "mK6W8Vhu7qQCb6hV"
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('HOSTNAME') or "postgresql://postgres:postgres@localhost:5432/playstore"
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or "postgresql://postgres:postgres@localhost:5432/playstore"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-MYSQL_HOST = "127.0.0.1"
-MYSQL_USER = "root"
-MYSQL_PASSWORD = "root"
-MYSQL_DB = "playstore"
-MYSQL_PORT = 3306
